1b702b16cb5dd99940df26b5e2e4be48e9b3bd34
Emmanuel Guefif <eguefif@fastmail.com>
2025-05-10 18:57:52 -0400

    Add struct for parameters

---
+++ src/ft_ssl.h
@@ -0,0 +1,21 @@
+#ifndef FT_SSL
+#include <stdio.h>
+#define true 1
+#define false 0
+#define EXIT_FAILURE 1
+
+typedef int boolean;
+
+typedef struct {
+  boolean p;
+  boolean q;
+  boolean r;
+  boolean s;
+} Flags;
+
+typedef struct {
+  char *command;
+  Flags flags;
+  char *file;
+} Params;
+#endif
---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include "ft_ssl.h"
+
+int main(int argc, char **argv) {
+  if (argc <= 1 || argc > 3) {
+    printf("usage: ft_ssl command [flag] [file/string]\n");
+  } else {
+    printf("Running program\n");
+  }
+  return 0;
+}
