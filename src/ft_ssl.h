5f7b86d3d0e2ca941b0bb8ea4f1887328a7310dc
Emmanuel Guefif <eguefif@fastmail.com>
2025-05-10 19:15:26 -0400

    Parse params

--- src/ft_ssl.h
+++ src/ft_ssl.h
@@ -1,5 +1,6 @@
 #ifndef FT_SSL
 #include <stdio.h>
+#include <string.h>
 #define true 1
 #define false 0
 #define EXIT_FAILURE 1
@@ -7,15 +8,15 @@
 typedef int boolean;
 
 typedef struct {
-  boolean p;
-  boolean q;
-  boolean r;
-  boolean s;
+    boolean p;
+    boolean q;
+    boolean r;
+    boolean s;
 } Flags;
 
 typedef struct {
-  char *command;
-  Flags flags;
-  char *file;
+    char *command;
+    Flags flags;
+    char *file;
 } Params;
 #endif
