8b0670374a6e6cee812a87a4b8eeb05c07a3ebf7
Emmanuel Guefif <eguefif@fastmail.com>
2025-05-11 12:29:27 -0400

    MD5: add MD buffer and auxilliary functions

--- src/md5.c
+++ src/md5.c
@@ -4,6 +4,10 @@
 void makeDigestFromPaddedTarget(char *digest, MD5Data *data);
 void padTarget(MD5Data *data);
 void pad(MD5Data *data);
+u32 F(u32 X, u32 Y, u32 Z);
+u32 G(u32 X, u32 Y, u32 Z);
+u32 H(u32 X, u32 Y, u32 Z);
+u32 I(u32 X, u32 Y, u32 Z);
 
 void runMD5(Params params) { printf("Running MD5\n"); }
 
@@ -53,4 +57,13 @@ void appendLength(MD5Data *data) {
 void makeDigestFromPaddedTarget(char *digest, MD5Data *data) {
     assert(data->paddedTargetSize * 8 % 512 == 0);
     bzero(digest, 16);
+    u32 A = 0x01234567;
+    u32 B = 0x89abcdef;
+    u32 C = 0xfedcba98;
+    u32 D = 0x76543210;
 }
+
+u32 F(u32 X, u32 Y, u32 Z) { return X & Y | (!X) & Z; }
+u32 G(u32 X, u32 Y, u32 Z) { return X & Z | Y & (!Z); }
+u32 H(u32 X, u32 Y, u32 Z) { return X ^ Y ^ Z; }
+u32 I(u32 X, u32 Y, u32 Z) { return Y ^ (X | (!Z)); }
