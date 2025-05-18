f11f012c9bd0fa3ab74f8be6281d12ded87d5fe1
Emmanuel Guefif <eguefif@fastmail.com>
2025-05-18 19:20:50 -0400

    Add logical functions

--- src/sha256.h
+++ src/sha256.h
@@ -1,6 +1,16 @@
 #ifndef SHA256
 #include "ft_ssl_types.h"
 
+#define ROTR(X, S) (((X) >> S) | ((X) << (32 - S)))
+#define ROTL(X, S) (((X) << S) | ((X) >> (32 - S)))
+
+#define CH(X, Y, Z) (((X) & ((Y)) ^ ((~X) & (Z)))
+#define MAJ(X, Y, Z) (((X) & (Y)) ^ ((X) & (Z)) ^ ((Y) & (Z)))
+#define BSIG0(X) (ROTR(X, 2) ^ ROTR(x, 13) ^ ROTR(X, 22))
+#define BSIG1(X) (ROTR(X, 6) ^ ROTR(x, 11) ^ ROTR(X, 25))
+#define SSIG0(X) (ROTR(X, 7) ^ ROTR(x, 18) ^ (X >> 3))
+#define SSIG1(X) (ROTR(X, 17) ^ ROTR(x, 19) ^ (X >> 10))
+
 typedef struct {
     u32 states[8];
     u8 buffer[64];
